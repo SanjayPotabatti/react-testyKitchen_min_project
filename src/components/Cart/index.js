7311b7b41da78910280f9f05d6269b36102bf98a
sanjay <sanjaysulochanap@gmail.com>
2022-01-24 13:13:13 +0530

    changes 7

---
+++ src/components/Cart/index.js
@@ -0,0 +1,35 @@
+import {Component} from 'react'
+
+import Header from '../Header'
+import Footer from '../Footer'
+
+import CartContext from '../../Context/CartContext'
+import EmptyCart from '../EmptyCart'
+import CartListView from '../CartListView'
+
+import './index.css'
+
+class Cart extends Component {
+  render() {
+    return (
+      <CartContext.Consumer>
+        {value => {
+          const {cartList} = value
+          const isCartEmpty = cartList.length === 0
+
+          return (
+            <>
+              <Header />
+              <div className="cart-container">
+                {isCartEmpty ? <EmptyCart /> : <CartListView />}
+              </div>
+              <Footer />
+            </>
+          )
+        }}
+      </CartContext.Consumer>
+    )
+  }
+}
+
+export default Cart
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,39 +1,81 @@
+import {Link, withRouter} from 'react-router-dom'
 import {HiViewList} from 'react-icons/hi'
+import Cookies from 'js-cookie'
+import CartContext from '../../Context/CartContext'
 import './index.css'
 
-const Header = () => (
-  <nav className="navbar navbar-expand-lg navbar-light navbarBg">
-    <div className="d-flex flex-row align-items-center p-1">
-      <img
-        src="https://res.cloudinary.com/dlhxfaljh/image/upload/v1642240090/testyKitchen/Group_7420_2_x9ybhz.png"
-        className="logo-image-header"
-        alt="website logo"
-      />
-      <h1 className="header-heading ml-2">Tasty Kitchen</h1>
-    </div>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarTogglerDemo01"
-      aria-controls="navbarTogglerDemo01"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <HiViewList />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <ul className="navbar-nav pr-5">
-        <li className="nav-item active navItemsHeading p-2">Home</li>
-        <li className="nav-item navItemsHeading p-2">Cart</li>
-        <li className="nav-item">
-          <button className="logout-button p-2" type="button">
-            Logout
-          </button>
-        </li>
-      </ul>
-    </div>
-  </nav>
-)
+const Header = props => {
+  const onClickLogout = () => {
+    Cookies.remove('jwt_token')
+    const {history} = props
 
-export default Header
+    history.replace('/login')
+  }
+
+  const renderCartItemsCount = () => (
+    <CartContext.Consumer>
+      {value => {
+        const {cartList} = value
+        const cartItemsCount = cartList.length
+
+        return (
+          <>
+            {cartItemsCount > 0 ? (
+              <span className="cart-count-badge">{cartList.length}</span>
+            ) : null}
+          </>
+        )
+      }}
+    </CartContext.Consumer>
+  )
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light navbarBg">
+      <div className="d-flex flex-row align-items-center p-1">
+        <Link to="/" className="nav-link">
+          <img
+            src="https://res.cloudinary.com/dlhxfaljh/image/upload/v1642240090/testyKitchen/Group_7420_2_x9ybhz.png"
+            className="logo-image-header"
+            alt="website logo"
+          />
+          <h1 className="header-heading ml-2">Tasty Kitchen</h1>
+        </Link>
+      </div>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarTogglerDemo01"
+        aria-controls="navbarTogglerDemo01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <HiViewList />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <ul className="navbar-nav pr-5">
+          <Link to="/" className="nav-link">
+            <li className="nav-item active navItemsHeading p-2">Home</li>
+          </Link>
+          <Link to="/cart">
+            <li className="nav-item navItemsHeading p-2">
+              Cart
+              {renderCartItemsCount()}
+            </li>
+          </Link>
+          <li className="nav-item">
+            <button
+              className="logout-button p-2"
+              type="button"
+              onClick={onClickLogout}
+            >
+              Logout
+            </button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default withRouter(Header)
